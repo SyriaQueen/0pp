4d3b8addf1ae451a1da8901ddd869b9ef7b9ade0
jad <bawabhjad123@gmail.com>
2026-02-09 21:27:12 +0300

    Update LoginResponse.java

--- app/src/main/java/com/halaqat/attendance/models/LoginResponse.java
+++ app/src/main/java/com/halaqat/attendance/models/LoginResponse.java
@@ -9,9 +9,28 @@ public class LoginResponse {
     @SerializedName("user")
     private User user;
     
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
+    // Constructors
+    public LoginResponse() {}
     
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public LoginResponse(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+    
+    // Getters and Setters
+    public String getToken() { 
+        return token; 
+    }
+    
+    public void setToken(String token) { 
+        this.token = token; 
+    }
+    
+    public User getUser() { 
+        return user; 
+    }
+    
+    public void setUser(User user) { 
+        this.user = user; 
+    }
 }
